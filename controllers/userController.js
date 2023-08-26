6d5b6acd542e0221c278f19a3e5a545e0e84463e
danzBraham <zidangit07@gmail.com>
2023-08-26 15:05:45 +0800

    Implement API endpoint to fetch all users
    - Create GET /api/users endpoint
    - Retrieve user data excluding sensitive information

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,22 @@
+import { StatusCodes } from 'http-status-codes';
+import { attachCookiesToResponse } from '../utils/index.js';
+import { BadRequestError, UnauthorizedError } from '../errors/index.js';
 import pool from '../db/connectDB.js';
 
 export const getAllUsers = async (req, res) => {
-  res.send('Get All Users');
+  const query = {
+    text: `SELECT user_id, username, email, role, created_at, updated_at
+            FROM users
+            WHERE role = $1`,
+    values: ['user'],
+  };
+  const { rows } = await pool.query(query);
+
+  res.json({
+    status: 'success',
+    message: 'success getting all users',
+    data: rows,
+  });
 };
 
 export const getSingleUser = async (req, res) => {
