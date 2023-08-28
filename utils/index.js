c929e13e2f9e7ecd7604e5c460ce9269a604dcd0
danzBraham <zidangit07@gmail.com>
2023-08-28 10:37:20 +0800

    Implement password utility functions
    - Create functions to hash and verify passwords securely
    - Enhance password management using bcrypt
    - Improve code organization and reusability

--- utils/index.js
+++ utils/index.js
@@ -1,3 +1,4 @@
 import { createJWT, verifyToken, attachCookiesToResponse } from './jwt.js';
+import { createPassword, verifyPassword } from './password.js';
 
-export { createJWT, verifyToken, attachCookiesToResponse };
+export { createJWT, verifyToken, attachCookiesToResponse, createPassword, verifyPassword };
---
+++ utils/password.js
@@ -0,0 +1,13 @@
+import bcrypt from 'bcryptjs';
+import { AuthenticationError } from '../errors/index.js';
+
+export const createPassword = async (password) => {
+  const salt = await bcrypt.genSalt(10);
+  return bcrypt.hash(password, salt);
+};
+
+export const verifyPassword = async (password, hashedPassword) => {
+  const isPasswordCorrect = await bcrypt.compare(password, hashedPassword);
+  if (!isPasswordCorrect) throw new AuthenticationError('Invalid password');
+  return isPasswordCorrect;
+};
