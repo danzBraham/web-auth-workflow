9d4556c61fcf5fe642057fcec08c89b27f6007ab
danzBraham <zidangit07@gmail.com>
2023-09-03 07:56:42 +0800

    Refactor Authentication Middleware Logic
    - Improve handling of access and refresh tokens
    - Streamline conditional logic for better readability
    - Maintain clear separation between token verification and database query

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,16 +1,44 @@
 import { AuthenticationError, AuthorizationError } from '../errors/index.js';
-import { verifyToken } from '../utils/jwt.js';
+import { verifyToken, attachCookiesToResponse } from '../utils/index.js';
+import pool from '../db/connectDB.js';
 
 export const authenticateUser = async (req, res, next) => {
-  const { token } = req.signedCookies;
-  if (!token) throw new AuthenticationError('Authentication failed: Missing token');
+  const { accessToken, refreshToken } = req.signedCookies;
 
   try {
-    const { userId, username, role } = verifyToken({ token });
-    req.user = { userId, username, role };
+    if (accessToken) {
+      const { userId, username, role } = verifyToken(accessToken);
+      req.user = { userId, username, role };
+    } else if (refreshToken) {
+      const {
+        userId,
+        username,
+        role,
+        refreshToken: payloadRefreshToken,
+      } = verifyToken(refreshToken);
+
+      const query = {
+        text: `SELECT is_valid FROM tokens
+                WHERE user_id = $1 AND refresh_token = $2`,
+        values: [userId, payloadRefreshToken],
+      };
+      const { rows } = await pool.query(query);
+      const tokenExist = rows[0];
+
+      if (!tokenExist || !tokenExist?.is_valid) {
+        throw new AuthenticationError('Authentication failed: Invalid token');
+      }
+
+      const userPayload = { userId, username, role };
+      attachCookiesToResponse({ res, userPayload, refreshToken: payloadRefreshToken });
+      req.user = userPayload;
+    } else {
+      throw new AuthenticationError('Authentication failed: No token provided');
+    }
+
     next();
   } catch (error) {
-    throw new AuthenticationError('Authentication failed: Invalid token');
+    throw new AuthenticationError(error.message);
   }
 };
 
--- utils/jwt.js
+++ utils/jwt.js
@@ -1,15 +1,19 @@
 import jwt from 'jsonwebtoken';
+import { AuthenticationError } from '../errors/index.js';
 
-export const createJWT = ({ payload }) => {
-  const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: process.env.JWT_LIFETIME });
-  return token;
+export const createJWT = ({ payload }) => jwt.sign(payload, process.env.JWT_SECRET);
+
+export const verifyToken = (token) => {
+  try {
+    return jwt.verify(token, process.env.JWT_SECRET);
+  } catch (error) {
+    throw new AuthenticationError('Authentication failed: Invalid token');
+  }
 };
 
-export const verifyToken = ({ token }) => jwt.verify(token, process.env.JWT_SECRET);
-
 export const attachCookiesToResponse = ({ res, userPayload, refreshToken }) => {
-  const accessTokenJWT = createJWT({ payload: { userPayload } });
-  const refreshTokenJWT = createJWT({ payload: { userPayload, refreshToken } });
+  const accessTokenJWT = createJWT({ payload: { ...userPayload } });
+  const refreshTokenJWT = createJWT({ payload: { ...userPayload, refreshToken } });
 
   const twoWeeks = 1000 * 60 * 60 * 24 * 14;
 
@@ -17,7 +21,7 @@ export const attachCookiesToResponse = ({ res, userPayload, refreshToken }) => {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
     signed: true,
-    maxAge: 1000,
+    maxAge: 1000 * 60 * 15,
   });
 
   res.cookie('refreshToken', refreshTokenJWT, {
